2d04e49215523153c53370a8df3a249dab827f4c
Sergent frank <105205686+Sergentfrank@users.noreply.github.com>
2022-06-20 09:14:45 +0100

    Add files via upload

---
+++ js tutorials/index.js
@@ -0,0 +1,24 @@
+const users = [
+    {name:"peter Akaliro", score: 95.9},
+    {name: "steve jobs", score: 50.1},
+    {name:"Emeka wonder", score: 48.7},
+    {name: "jane Doe", score: 12.5, gender:"female"},
+];
+
+// const newUsers = users.map((singleValue)=>{
+//     return{lastName: singleValue.name.split(" ")[1], score: singleValue.score};
+// });
+
+// console.log(newUsers);
+
+
+
+// const filteredUsers = users.filter(
+//     (singleValue) => singleValue.gender ==="female"
+// );
+
+// console.log(filteredUsers);
+
+
+// const passedUsers = users.filter((user)=> user.score >=50);
+// console.log(passedUsers);
